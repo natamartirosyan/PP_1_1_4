f5687e2c7dfecd5f4af950eaaa4f4623a14d0481
Natalya Martirosyan <nata.martirosyan@gmail.com>
2023-11-29 00:19:16 +0300

    commit branch1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,19 +1,30 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.mapping.Table;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
-
-    }
-
+    Configuration config = new Configuration().addAnnotatedClass(User.class);
+    SessionFactory sessionFactory = config.buildSessionFactory();
+    Session session = sessionFactory.getCurrentSession();
 
     @Override
     public void createUsersTable() {
-        //PreparedStatement
+        try {
+            session.beginTransaction();
+
+            Table table = new Table("User");
+            session.save(table);
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
     }
 
     @Override
@@ -23,12 +34,34 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try {
+            session.beginTransaction();
 
+            User user = new User(name, lastName, age);
+
+            session.save(user);
+
+            session.getTransaction().commit();
+        }
+        finally {
+            sessionFactory.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try {
+            session.beginTransaction();
 
+            User user = session.get(User.class, id);
+
+            session.delete(user);
+
+            session.getTransaction().commit();
+        }
+        finally {
+            sessionFactory.close();
+        }
     }
 
     @Override
